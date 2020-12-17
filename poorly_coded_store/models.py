290d557e94e3a8ad7956435d83ea5242e861fa94
opusvar <sprojdev@gmail.com>
2020-12-17 09:39:07 -0500

    finished redirect

--- poorly_coded_store/models.py
+++ poorly_coded_store/models.py
@@ -6,8 +6,20 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __repr__(self):
+        return f"<Product Object: Price: {self.price} | {self.description}>"
+
 class Order(models.Model):
     quantity_ordered = models.IntegerField()
     total_price = models.DecimalField(decimal_places=2, max_digits=6)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __repr__(self):
+        return f"Order Object: id: {self.id} | quantity_ordered: {self.quantity_ordered} | total price {self.total_price}"
+
+class User_Order(models.Model):
+    total_spending = models.DecimalField(decimal_places=2, max_digits=6)
+    
+    def __repr__(self):
+        return f"<User_Order Object: id: {self.id} | total_spending: {self.total_spending} | "
--- poorly_coded_store/views.py
+++ poorly_coded_store/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Order, Product
+from django.shortcuts import render, redirect
+from .models import Order, Product, User_Order
 
 def index(request):
     context = {
@@ -7,10 +7,26 @@ def index(request):
     }
     return render(request, "store/index.html", context)
 
-def checkout(request):
-    quantity_from_form = int(request.POST["quantity"])
-    price_from_form = float(request.POST["price"])
-    total_charge = quantity_from_form * price_from_form
+def process_checkout(request):
+    request.session["quantity"] = int(request.POST["quantity"])
+    request.session["price"] = float(request.POST["price"])
+    request.session["total_charge"] = float(request.session["quantity"] * request.session["price"])
     print("Charging credit card...")
-    Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
-    return render(request, "store/checkout.html")
+    Order.objects.create(quantity_ordered=request.session["quantity"], total_price=request.session["total_charge"])
+    return redirect("/checkout/") 
+
+
+def checkout(request):
+    quantity_from_form = request.session["quantity"]
+    price_from_form = request.session["total_charge"]
+    total_charge = request.session["total_charge"]
+    total_spent = 0
+    for Order.total_price in Order.objects.all():
+        print(Order.total_price)
+    context = {
+        "quantity_from_form" : quantity_from_form,
+        "price_from_form" : price_from_form,
+        "total_charge": total_charge,
+        "total_spent" : total_spent
+    }
+    return render(request, "store/checkout.html", context) 
